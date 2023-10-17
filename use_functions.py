6bf6e81cdb6673da1849163c51736767fa8b3729
demon <dima@n-line.ru>
2023-10-17 12:36:16 +0300

    add dz fdfes

--- use_functions.py
+++ use_functions.py
@@ -34,19 +34,54 @@
 Для реализации основного меню можно использовать пример ниже или написать свой
 """
 
+money_count = 0
+balance = 0
+history_buy = []
+
+
+def add_money(cash, balance):
+    balance = cash + balance
+    return balance
+
+
+def history_pokuppok(cash, item_to_buy):
+    history_buy.append((cash, item_to_buy))
+    return history_buy
+
+
+
+#history_pokuppok(100, "eda")
+#history_pokuppok(200, "edasda")
+#print(history_buy)
+
+def buy_item(cash, balance):
+    if cash > balance:
+        print("Денег не хватает")
+    else:
+        item_to_buy = str(input("Введите название покупки: "))
+        history_pokuppok(cash, item_to_buy)
+        balance = (balance - cash)
+        # history_buy.append(item_to_buy, balance)
+        #print(history_buy)
+        return balance
+
 while True:
     print('1. пополнение счета')
     print('2. покупка')
     print('3. история покупок')
     print('4. выход')
 
-    choice = input('Выберите пункт меню')
+    choice = input('Выберите пункт меню: ')
     if choice == '1':
-        pass
+        cash = int(input("Введите сумму для пополнения: "))
+        balance = add_money(cash, balance)
+        print("Ваш баланс: ", balance)
     elif choice == '2':
-        pass
+        cash = int(input("Введите сумму покупки: "))
+        balance = buy_item(cash, balance)
     elif choice == '3':
-        pass
+        for history_buy_item in history_buy:
+            print(f"Покупка: {history_buy_item[1]} на сумму, {history_buy_item[0]} ")
     elif choice == '4':
         break
     else:
